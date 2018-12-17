82cf8bbb7d4305b272aecd56ea035cdf9c5eca87
livexia <istillalive@live.cn>
2018-12-17 19:22:20 +0800

    modify install-openvm.sh

--- kali/maintenance/install-openvm.sh
+++ kali/maintenance/install-openvm.sh
@@ -1,5 +1,5 @@
 #/bin/sh
-sudo pt update && apt -y full-upgrade
+sudo apt update && apt -y full-upgrade
 
 # Reboot now in case you have updated to a new kernel. Once rebooted:
 sudo apt -y --reinstall install open-vm-tools-desktop fuse
